676b53f7578ae68d90b8e2400b008369ad72ed10
maciejgalant <166690492+maciejgalant@users.noreply.github.com>
2024-04-30 14:42:23 +0200

    wip:add remove function

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 from models.data import users
-from utils.crud import show_users,add_new_user, search_user, remove_users
-
-
-
-
+from utils.crud import show_users, add_new_user, search_user, remove_user
 
 if __name__ == "__main__":
-     print("Witaj użytkowniku")
-     while True:
+    print("Witaj użytkowniku")
+    while True:
 
-         print("Menu:")
-         print("0. Zakończ program")
-         print("1. wyświetl co u znajomych")
-         print("2. dodaj użytkownika")
-         print("3. znajdź użytkownika")
-         print("4. usuń użytkownika")
-         menu_option:str=input("Dokonaj wyboru:")
-         if menu_option == "0":
-             break
-         if menu_option == "1":
-             show_users(users)
-         if menu_option == "2":
-             show_users(users)
-             add_new_user(users)
-         if menu_option == "3":
-             search_user(users)
-         if menu_option == "4":
-             remove_user(users)
-
-
-
-
-
-
-
-
-
+        print("Menu:")
+        print("0. Zakończ program")
+        print("1. wyświetl co u znajomych")
+        print("2. dodaj użytkownika")
+        print("3. znajdź użytkownika")
+        print("4. usuń użytkownika")
+        menu_option: str = input("Dokonaj wyboru:")
+        if menu_option == "0":
+            break
+        if menu_option == "1":
+            show_users(users)
+        if menu_option == "2":
+            show_users(users)
+            add_new_user(users)
+        if menu_option == "3":
+            search_user(users)
+        if menu_option == "4":
+            remove_user(users)
